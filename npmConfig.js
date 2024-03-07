99b9b544a8cb2f42b7075f40b7638f7dbd6d4b76
ouyang <stwflyfox@163.com>
2024-03-07 15:28:07 +0800

    更新配置

--- npmConfig.js
+++ npmConfig.js
@@ -1,6 +1,6 @@
 //存放私库包的命名空间
 export const scopes = [
-    '@xianzou', '@lijq'
+    '@shqy'
 ];
 /****
  * 私库地址，代理端口会解析url的端口号
@@ -9,7 +9,7 @@ export const scopes = [
  * 拉取一些npm的包会返回302的情况，unpkg暂时没有处理，会不会和本地的npm源有关？
  ***/
 
-export const privateNpmRegistryURL = 'http://192.168.0.77:7788';
+export const privateNpmRegistryURL = 'http://127.0.0.1:4873';
 
 //互联网npm地址
 export const publicNpmRegistryURL = 'http://registry.npmjs.org';
